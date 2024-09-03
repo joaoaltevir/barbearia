0973cf3aec27adef2126352e86d91e9bfeac9461
João Neto <j.altevirneto@gmail.com>
2024-09-03 11:46:14 -0300

    feat: add on github

--- src/models/atendimento.js
+++ src/models/atendimento.js
@@ -2,11 +2,11 @@ let nextID = 1;
 const clienteController = require('../controllers/cliente.js');
 const servicoController = require('../controllers/servico.js');
 
+const validarServicos = servicos => servicos.every(servico => servicoID == )
 
 const model = (body, id = nextID++) => {
     if(
         Array.isArray(body.servicos) &&
-        servicoController.show(body.servico.servicoID) &&
         body.horarioInicio != undefined &&
         body.horarioInicio != "" &&
         body.statusPagamento != undefined &&
