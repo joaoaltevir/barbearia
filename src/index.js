5cb8fdcc79d93919f9852c74734b1dfc57e4a270
João Neto <j.altevirneto@gmail.com>
2024-09-03 09:09:15 -0300

    feat & refact: criado interação com usuario (atendimento) e refatoração por rotas do crud de barbeiro e servico

--- src/index.js
+++ src/index.js
@@ -3,75 +3,28 @@ const redeRouter = require("./routes/rede.js");
 const clienteRouter = require("./routes/cliente.js");
 const barbeariaRouter = require("./routes/barbearia.js");
 const usuarioRouter = require("./routes/usuario.js");
+const barbeiroRouter = require("./routes/barbeiro.js");
+const servicoRouter = require("./routes/servico.js");
+const atendimentoRouter = require("./routes/atendimento.js");
 const app = express();
 const port = 3000
 
 app.use(express.json());
 
 //GERENCIAMENTO DE USUARIOS
-
 app.use("/usuario", usuarioRouter);
-
 //GERENCIAMENTO DE CLIENTE
-
 app.use("/cliente", clienteRouter);
-
 //GERENCIAMENTO DE BARBEARIA
-
 app.use("/barbearia", barbeariaRouter);
-
 //GERENCIAMENTO DE REDES
-
 app.use("/rede", redeRouter);
-
 //GERENCIAMENTO BARBEIRO
-
-app.post("/barbeiro", (req, res) => {
-    const barbeiro = req.body
-    const code = barbeiroController.store(barbeiro)
-    res.status(code).json()
-})
-app.get("/barbeiro",(req, res)=> {
-   res.json(barbeiroController.index());
-})
-app.get("/barbeiro/:id", (req, res)=>{
-   const code = barbeiroController.show(req.params.id);
-   res.status(code).json();
-})
-app.put("barbeiro/:id", (req, res) =>{
-   const barbeiro = req.body;
-   const code = barbeiroController.update(barbeiro, req.params.id);
-   res.status(code).json();
-})
-app.delete("barbeiro/:id", (req,res)=>{
-   const code = barbeiroController.destroy(req.params.id);
-   res.status(code).json();
-})
-
-
+app.use("/barbeiro", barbeiroRouter);
 //GERENCIMANENTO DE SERVIÇO
-
-app.post("/servico", (req, res) => {
-    const servico = req.body
-    const code = servicoController.store(servico)
-    res.status(code).json()
-})
-app.get("/servico",(req, res)=> {
-   res.json(servicoController.index());
-})
-app.get("/servico/:id", (req, res)=>{
-   const code = servicoController.show(req.params.id);
-   res.status(code).json();
-})
-app.put("servico/:id", (req, res) =>{
-   const servico = req.body;
-   const code = servicoController.update(servico, req.params.id);
-   res.status(code).json();
-})
-app.delete("servico/:id", (req,res)=>{
-   const code = servicoController.destroy(req.params.id);
-   res.status(code).json();
-})
+app.use("/servico", servicoRouter);
+//GERENCIAMENTO ATENDIMENTO
+app.use("/atendimento", atendimentoRouter);
 
 app.listen(port, () => {
     console.log(`server running in ${port} port`)
